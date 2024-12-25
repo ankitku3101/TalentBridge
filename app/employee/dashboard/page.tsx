3f50902caa2c114f1cbe53c849a23d9ceadcd376
royanish123 <anish.anandroy@gmail.com>
2024-12-25 13:11:15 +0530

    initialization of employee dashboard

---
+++ app/employee/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import EmployeeDashboard from '@/pages/employeeDashboard';
+ 
+
+const Dashboard = () => {
+  return (
+    <div>
+      <EmployeeDashboard />
+    </div>
+  );
+};
+
+export default Dashboard;
