71dfff6d29c8dbd9fef5aa6474237b88359d62ab
Adyasha Nanda <adyashananda105@gmail.com>
2024-12-26 17:06:31 +0530

    emp dashboard confict

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      firstName: string | null | undefined;
       id: string; 
       name?: string | null;
       email?: string | null;
