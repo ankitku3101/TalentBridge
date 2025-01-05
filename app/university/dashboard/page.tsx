c176239570d3fff091b0057abf47c4ca160f07bc
ankitku3101 <ankit.ku3101@gmail.com>
2025-01-05 10:15:28 +0530

    Added comps to Unversity Dashboard

--- app/university/dashboard/page.tsx
+++ app/university/dashboard/page.tsx
@@ -2,13 +2,22 @@ import UniversityDashboard from '@/pages/universityDashboard';
 import Analytics from '../sections/Analytics';
 import EmployerCount from '../sections/EmployerCount';
 import SkillGap from '../sections/SkillGap';
+import FetchStudents from '../sections/FetchStudents';
  
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className='flex flex-col'>
+      <div className='h-40 p-8'>
+        <h1 className='text-center font-semibold text-5xl tracking-tight'>
+          University Dashboard
+        </h1>
+      </div>
       <div className='h-screen'>
         <Analytics />
+      </div>
+      <div className='h-screen'>
+        <FetchStudents />
       </div>
       <div className='h-screen'>
         <EmployerCount />
